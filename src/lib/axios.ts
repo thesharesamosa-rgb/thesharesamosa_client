72712738b7bd5f2be7e4b87727f95b7a06c93c4c
Kunal Kumar <kunal34255@gmail.com>
2025-09-02 12:35:20 +0530

    keyword removes

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.VITE_PROD === "production"
       ? import.meta.env.VITE_API_BASE_URL
-      : "http://localhost:8080/api/v1",
+      : "http://localhost:8080",
   withCredentials: true,
 });
